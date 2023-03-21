69343ae94694de37e07589c0d268e1f030ec2c87
vladimirivn <vladimiriv@hotmail.com>
2023-03-21 16:30:50 +0300

    Задание №1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,14 +3,25 @@ import java.sql.*;
 public class Application {
     public static void main(String[] args) {
 
-        String sql = "SELECT * FROM employee WHERE id = (?)";
+        String sql = "SELECT first_name, last_name, gender, city_name " +
+                     "FROM employee " +
+                     "LEFT JOIN city USING(city_id) " +
+                     "WHERE id = (?)";
 
         try (Connection connection = getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)){;
-             statement.setInt(1, 5);
+             statement.setInt(1, 4);
              ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                System.out.println(resultSet.getString(2));
+                String firstName = "First name: " + resultSet.getString(1);
+                String lastName = "Last name: " + resultSet.getString(2);
+                String gender = "Gender: " + resultSet.getString(3);
+                String city = "City: " + resultSet.getString(4);
+
+                System.out.println(firstName);
+                System.out.println(lastName);
+                System.out.println(gender);
+                System.out.println(city);
             }
         } catch (SQLException sqlException) {
             throw new RuntimeException(sqlException);
